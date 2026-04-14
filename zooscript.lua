218441e8b49e4d6e7fa070ba79b7505102696fcd
Tegar Jatu Ramadhani <tegarjr634@gmail.com>
2026-04-14 10:42:45 +0700

    Update zooscript.lua

--- zooscript.lua
+++ zooscript.lua
@@ -116,7 +116,7 @@ MainTab:CreateButton({
     Callback = function()
         local success, err = pcall(function()
             -- Sesuai eksperimenmu, "CelesteEgg" memicu x3
-            ProductBuyRF:InvokeServer("CelesteEgg")
+            ProductBuyRF:InvokeServer("CelesteEgg_x3")
         end)
         if not success then warn("Error Celeste:", err) end
     end,
