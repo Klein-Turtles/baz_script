ecc89e7eb53977f22160f41a5a725940dbbc88d1
Tegar Jatu Ramadhani <tegarjr634@gmail.com>
2026-04-14 10:22:33 +0700

    Update zooscript.lua

--- zooscript.lua
+++ zooscript.lua
@@ -213,7 +213,7 @@ MainTab:CreateButton({
          
          -- Argumen spesifik untuk Celeste Egg x10
          local args = {
-            "CelesteEgg_x1"
+            "CelesteEgg_x10"
          }
 
          print("Mengirim request pembelian Celeste Egg x10 ke Server...")
